a8240852b5629f1509e42c349dfc387e3fe6751f
Malte Münchert <malte.muenchert@gmx.net>
2015-12-02 23:17:04 +0100

    Attach orphaned ELSE blocks found in XML files < 2015

--- src/node/conditional_node.rb
+++ src/node/conditional_node.rb
@@ -1,9 +1,17 @@
 class ConditionalNode < Node
   def initialize(element)
     @cond_expr = element.attr('expr')
-    @then = SourceNode.new(element.xpath('./THEN').children)
-    @else = SourceNode.new(element.xpath('./ELSE').children)
+    @then = SourceNode.new(element.xpath('./THEN/*'))
+    @else = SourceNode.new(element.xpath('./ELSE/*'))
     super()
+  end
+
+  def attach_else(elements)
+    if @else.lines.count > 0
+      raise "Cannot attach another ELSE block to this conditional statement: #{elements}"
+    else
+      @else = SourceNode.new(elements)
+    end
   end
 
   def render
@@ -11,12 +19,13 @@ class ConditionalNode < Node
     ident do
       output @then
     end
-    output('else')
-    ident do
-      output @else
+    if @else.lines.count > 0
+      output('else')
+      ident do
+        output @else
+      end
     end
     output('end')
-    #linefeed
     output_buffer
   end
 end
--- src/node/source_node.rb
+++ src/node/source_node.rb
@@ -1,15 +1,27 @@
 class SourceNode < Node
+  attr_accessor :lines
   def initialize(elements)
-    @children = []
+    @lines = []
+    lc = nil
     elements.each do |element|
       if element.name == 'EXECUTE'
-        @children << MethodCallNode.new(element)
+        @lines << MethodCallNode.new(element)
+        lc = nil
       elsif element.name == 'EVAL'
-        @children << ExprNode.new(element)
+        @lines << ExprNode.new(element)
+        lc = nil
       elsif element.name == 'IF'
-        @children << ConditionalNode.new(element)
+        @lines << ConditionalNode.new(element)
+        lc = @lines.last
+      elsif element.name == 'ELSE'
+        if lc
+          puts 'WARNING: Orphaned ELSE block found, but assigning it to previous IF statement'
+          lc.attach_else(element.xpath('./*'))
+        else
+          raise UnknownTagError.new("Orphaned ELSE block found: #{element.inspect}")
+        end
       elsif Node.is_comment?(element)
-        @children << CommentNode.new(element)
+        @lines << CommentNode.new(element)
       elsif Node.is_text?(element)
         if element.inner_text.strip != ''
           raise UnknownTextError.new(element.inner_text)
@@ -22,8 +34,8 @@ class SourceNode < Node
   end
 
   def render
-    @children.each do |child|
-      output(child)
+    @lines.each do |line|
+      output(line)
     end
     output_buffer
   end
